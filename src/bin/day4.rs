cb6176a5e1b792653b99d84e9ced0d4c83dc93c8
Peter Keyzers <peter.keyzers@gmail.com>
2019-12-04 22:34:32 +0000

    Day 4 solution looks much nicer now. Thanks to the amazing @annabee !

--- src/bin/day4.rs
+++ src/bin/day4.rs
@@ -1,49 +1,46 @@
-fn get_matches(start: i32, end: i32, restrict_length: bool) -> i32 {
-    let mut matches = 0;
+use counter::Counter;
 
-    for candidate in start..=end {
-        let str_candidate = candidate.to_string();
-        let mut rle_data: Vec<i32> = Vec::new();
-        let mut str_iterator = str_candidate.chars();
-        let mut previous_char = str_iterator.next().unwrap();
-        let mut previous_char_count = 1;
-        let mut increasing = true;
+fn increasing(code: i32) -> bool {
+    let str_code = code.to_string();
+    let code_iterator = str_code.chars();
+    let mut next_code_iterator = str_code.chars();
+    next_code_iterator.next();
 
-        for current_char in str_iterator {
-            if current_char != previous_char {
-                if current_char < previous_char {
-                    increasing = false;
-                }
-
-                rle_data.push(previous_char_count);
-                previous_char = current_char;
-                previous_char_count = 1;
-            } else {
-                previous_char_count += 1;
-            }
-        }
-
-        rle_data.push(previous_char_count);
-
-        if increasing {
-            if restrict_length {
-                if rle_data.iter().filter(|&&n| n == 2).count() > 0 {
-                    matches += 1;
-                }
-            } else {
-                let counter = rle_data.iter().filter(|&&n| n > 1).count();
-                if counter > 0 {
-                    println!("{}", str_candidate);
-                    matches += 1;
-                }
-            }
+    for pair in code_iterator.zip(next_code_iterator) {
+        if pair.0 > pair.1 {
+            return false;
         }
     }
 
-    matches
+    true
+}
+
+fn digit_count(code: i32, strict: bool) -> bool {
+    let str_code = code.to_string();
+    let counter = str_code.chars().collect::<Counter<_>>();
+
+    if strict {
+        return counter.values().filter(|&&n| n == 2).count() > 0;
+    } else {
+        return counter.values().filter(|&&n| n > 1).count() > 0;
+    }
 }
 
 fn main() {
-    println!("{}", get_matches(382345, 843167, false));
-    println!("{}", get_matches(382345, 843167, true));
+    let candidates = (382345..=843167).collect::<Vec<_>>();
+
+    let part1 = candidates
+        .iter()
+        .map(|&n| increasing(n) && digit_count(n, false))
+        .filter(|&n| n)
+        .count();
+
+    let part2 = candidates
+        .iter()
+        .map(|&n| increasing(n) && digit_count(n, true))
+        .filter(|&n| n)
+        .count();
+
+    println!("{}", part1);
+    println!("{}", part2);
 }
